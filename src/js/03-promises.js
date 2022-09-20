3db8553a408de44a1af4450fa0768664ea142f14
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-09-20 11:10:55 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault;
+  const { delay, step, amount } = event.target.elements;
+
+  let delayVal = Number(delay.value);
+  let stepVal = Number(step.value);
+  let amountVal = Number(amount.value);
+
+  for (let i = 1; i <= amountVal; i += 1) {
+    createPromise(i, delayVal)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delayVal += stepVal;
   }
+};
+
+function createPromise(position, delay) {
+
+  return new Promise((resolve, reject) => {
+    
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
