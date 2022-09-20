57e1e62544f10ae358a7cfbd709fbcacda24dbd5
Nadiia23 <108667064+Nadiia23@users.noreply.github.com>
2022-09-20 12:08:05 +0300

    Add Notiflix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
-// Описаний в документації
+import Notiflix from 'notiflix';
 import flatpickr from "flatpickr";
+
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
@@ -27,7 +28,7 @@ const options = {
     minuteIncrement: 1,
     onOpen() {
         timerInterval && clearInterval(timerInterval)
-  },
+    },
     onClose(selectedDates) {
         if (selectedDates[0] < new Date()) {
             Notiflix.Notify.warning('Please choose a date in the future');
@@ -84,7 +85,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
